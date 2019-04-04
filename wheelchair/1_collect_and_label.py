7d3bb2efd35194ceaa92b7b71fc0af522f5e4c0d
Rosan Foppen <rosanfoppen@gmail.com>
2019-04-04 15:25:29 +0200

    test sitting data

--- wheelchair/1_collect_and_label.py
+++ wheelchair/1_collect_and_label.py
@@ -17,9 +17,9 @@ THING_ID = os.environ['THING_ID']
 THING_TOKEN = os.environ['THING_TOKEN']
 
 # Sitting classes
-CLASSES = ["Not Sitting", "Jelle", "Rosan", "William"]
+CLASSES = ["No One", "Jelle", "Rosan", "William"]
 
-LABEL_PROP_NAME = "Persona"
+LABEL_PROP_NAME = "sittingdata"
 DATA_PROP_NAME = "fsr"
 
 # How many samples do we want for each class
